be17bd2592e2fab92859c78227383855df72a2e4
hunain631 <hunainsaeed631@gmail.com>
2021-03-30 01:26:37 +0500

    complex queries done One funciton for all boolean queries simple and complex

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 bmr.main()
 
 app.add_url_rule('/', view_func=route.hello_world)
-app.add_url_rule('/<name>', view_func=route.name)
 app.add_url_rule('/simplequery/<query>', view_func=route.SimpleQuery)
+app.add_url_rule('/booleanquery/<query>', view_func=route.queryType)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- route.py
+++ route.py
@@ -1,13 +1,11 @@
 import bmr
 import json
+from operator import eq
+
+precedence = {'or': 1, 'and': 2, 'not': 3 }
 
 def hello_world():
     return json.dumps(bmr.i_index)
-
-
-def name(name):
-    return "Hello, "+name
-
 
 def findWord(word):
     try:
@@ -15,6 +13,95 @@ def findWord(word):
     except:
         print('"%s" is not a key in the dictionary' % word)
         return set()
+
+def checkPrecedence(stack, q):
+    try:
+        if precedence[q] <= precedence[stack[-1]]:
+            return True
+        else:
+            return False
+    except KeyError: 
+        return False
+
+def infixToPostfix(query):
+    op = []
+    post = []
+    # convert infix to post fix
+    for q in query:
+        if q not in precedence.keys():   # not operator then appent term
+            post.append(q)
+        else:
+            while op and checkPrecedence(op, q):
+                post.append(op.pop())
+            op.append(q)
+    while op:
+        post.append(op.pop())
+    
+    return post
+
+def BooleanQuery(query):
+    query = bmr.removePunctuation(query.lower()).split()
+    query = [bmr.ps.stem(word) for word in query]
+
+    if len(query) == 1:
+        print("normal case", query)
+        result = findWord(query[0])
+        if result:
+            print(sorted(list(result), key=int))
+            return json.dumps(sorted(list(result), key=int), indent=4)
+        else:
+            print("No document found!")
+            return ("No document found!")
+
+    post = infixToPostfix(query)
+    output = []
+    print("complex case", query)
+    for p in post:
+        if p not in precedence.keys():
+            output.append(findWord(p))
+        elif precedence[p] == 3:
+            output.append(set(bmr.docid) - output.pop())
+        elif precedence[p] == 2:
+            a = output.pop()
+            output.append(a.intersection(output.pop()))
+        elif precedence[p] == 1:
+            a = output.pop()
+            output.append(a.union(output.pop()))
+    
+    if output:
+        print(sorted(list(output[0]), key=int))
+        return json.dumps(sorted(list(output[0]), key=int), indent=4)
+    else:
+        return ("No document found!")
+
+# 0 = term, 1=or, 2=and, 3=not
+def isValidQuery(test):
+    position = [(1,2), (2,1), (3,1), (3,2), (0, 3)]
+    # adjecent values should not be euqal or same eg1. "term term" not allowed eg2. "and and" not allowed
+    if any(map(eq, test, test[1:])):
+        return False
+    # query shouldn't start with these two word "and" "or" and shouldn't end with operator
+    elif (test[0] == 1 or test[0] == 2) or (test[-1] == 1 or test[-1] == 2 or test[-1] == 3):
+        return False
+    # and or shouldn't be adjecent in any order 2nd "and, or" shouldn't come after "not"
+    # 3rd "not" shouldn't come after term
+    elif any(item in position for item in zip(test, test[1:])):
+        return False
+    else:
+        return True
+
+
+def queryType(query):
+    if '/' in query:
+        return ("proximity query")
+    else:
+        query = query.replace("-", " and ").replace("—", " and ")
+        test = [precedence.get(x, 0) for x in query.split()]
+        if isValidQuery(test):
+            return BooleanQuery(query)
+        else:
+            return ("Query is Invalid")
+
 
 # TODO remove stopwords from query
 def SimpleQuery(query):
@@ -44,10 +131,7 @@ def SimpleQuery(query):
     elif ('or' not in query) and ('and' not in query) and ('not' == query[0]):
         print("not query run")
         s1 = findWord(query[1])
-        if s1:
-            result = set(bmr.docid) - s1
-        else:
-            result = bmr.docid
+        result = set(bmr.docid) - s1
 
     if result:
         return json.dumps(sorted(list(result), key=int), indent=4)
